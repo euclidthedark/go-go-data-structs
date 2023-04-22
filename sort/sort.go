fb550d37ba363bd210d6fbe73f27c5b958a61b5f
euclidthedark <euclidthedark@gmail.com>
2023-04-21 23:01:27 -0400

    finish up bubble sort test.

--- sort/sort.go
+++ sort/sort.go
@@ -1,6 +1,24 @@
 package sort
 
-func BubbleSort(s *[]int) int {
-    return 0
+import (
+	"github.com/go-go-data-structs/utils"
+)
+
+func BubbleSort(s []int) []int {
+    iters := 0
+
+    for iters < (len(s) - 1) {
+        for i,x := range s[:(len(s)-1)] {
+            elementTwo := s[i+1]
+
+            if x > elementTwo {
+                utils.SwapFromSlice(s, i)
+            }
+        }
+
+        iters+=1
+    }
+
+    return s
 }
 
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -2,12 +2,20 @@ package sort
 
 import "testing"
 
+var sliceToTestSortFromLeastToGreatest = []int{10,9,8,7,6,5,4,3,2,1}
+var expedtedLeastToGreatestSortedSlice = []int{1,2,3,4,5,6,7,8,9,10}
+
 func Test_bubbleSort (t *testing.T) {
-    s := []int{}
+    actualSliceResult := BubbleSort(sliceToTestSortFromLeastToGreatest)
 
-    result := BubbleSort(&s)
-
-    if result != 0 {
-        t.Error("The test failed with result ", result)
+    for i,x := range expedtedLeastToGreatestSortedSlice {
+        if x != actualSliceResult[i] {
+            t.Errorf(
+                "Expected bubble sort to have value %d in index %d. Instead it returns %d.\n",
+                expedtedLeastToGreatestSortedSlice[i],
+                i,
+                actualSliceResult[i],
+            )
+        }
     }
 }
